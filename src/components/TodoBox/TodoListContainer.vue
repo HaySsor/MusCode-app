5236b63a86544245a18dd41dd48114cf3cbd050e
Przemek Hussar <hayss007@gmail.com>
2023-05-19 19:06:52 +0200

    fix list bug

--- src/components/TodoBox/TodoListContainer.vue
+++ src/components/TodoBox/TodoListContainer.vue
@@ -20,9 +20,9 @@ export default {
   components: {TodoListUlList, TodoAddNewTaskInput, TodoHeaderContainer},
   setup() {
     const tasksList = ref([
-      {text: 'Lorem ipsum dolor sit amet.', done: true, id: 93},
-      {text: 'consectetur adipisicing elit. Quasi, ipsa', done: true, id: 93},
-      {text: 'Quasi, ipsa optio molestias voluptas', done: true, id: 93},
+      {text: 'Lorem ipsum dolor sit amet.', done: false, id: 93},
+      {text: 'consectetur adipisicing elit. Quasi, ipsa', done: false, id: 93},
+      {text: 'Quasi, ipsa optio molestias voluptas', done: false, id: 93},
     ]);
 
     const addNewTaskToList = (newTaskObject) => {
